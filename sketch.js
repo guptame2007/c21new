0bc829d6bbc1ba38cfec58566b719619b66b3711
guptame2007 <85294609+guptame2007@users.noreply.github.com>
2021-07-05 17:34:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+var grid = 50; 
+var width = 1366; 
+var carGroup1,logGroup1; 
+var grassHeight = 100;
+ var gameState = "play"; 
+ var carAnimation, logAnimation, playerAnimation;
+  var school;
+  function preload() {
+
+
+   } 
+  
+  function setup() 
+  {
+     createCanvas(1366,2700); 
+    carGroup1 = new Group(); 
+    logGroup1 = new Group(); 
+    for (var i=0; i<6; i++)
+     { 
+      var bottomGrass1 = createSprite(683, height-50-(i*400), width, grassHeight); 
+      if(i%2===0)
+       {
+         var road = createSprite (683, height-150-(i*400)- grassHeight, width, 300);
+         road.shapeColor="black";
+         } 
+         bottomGrass1.shapeColor = "grey";
+         }
+          for(var i =0; i<40 ; i++)
+           {
+              cars = new Car(2); 
+            carGroup1.add(cars.spt);
+           }
+           } 
+            function draw() 
+            {
+              background("skyblue"); 
+              function KeyPressed()
+              {
+                if(keyCode == UP_ARROW)
+                {
+                  Player.move(0,-2)
+                } else if(keyCode == DOWN_ARROW)
+                {
+                  Player.move(0,2)
+                }else if(keyCode == LEFT_ARROW)
+                {
+                  Player.move(-2,0)
+                }else if(keyCode == RIGHT_ARROW)
+                {
+                  Player.move(2,0)
+                }
+
+              }
+            drawSprites(); 
+          }
